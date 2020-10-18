d00c5d7a39b35435bee02078b1ca4a80d9f6e18d
Krystof Spiller <s200360@student.dtu.dk>
2020-10-18 12:36:39 +0200

    Further simplification to the graph, now using only 2 + n + m of total vertices for the graph

--- week-6/mandatory-exercise/christmas-trees.py
+++ week-6/mandatory-exercise/christmas-trees.py
@@ -5,49 +5,47 @@ import collections
 
 # read the input file
 n, m = [int(s) for s in sys.stdin.readline().split()]
-empty_squares = 0
 trees_matrix = [[None for _ in range(m)] for _ in range(n)]
 
 for i in range(n):
     line = [int(s) for s in sys.stdin.readline().split()]
-    empty_squares += m - line[0]
     for j in range(1, len(line)):
         trees_matrix[i][line[j]] = 1
 
 # define graph classes
 class FlowEdge:
     def __init__(self, v, w, capacity):
-        self.from_node = v
-        self.to_node = w
+        self.from_vertex = v
+        self.to_vertex = w
         self.capacity = capacity
         self.flow = 0
 
     def other(self, vertex):
-        if vertex == self.from_node:
-            return self.to_node
-        elif vertex == self.to_node:
-            return self.from_node
+        if vertex == self.from_vertex:
+            return self.to_vertex
+        elif vertex == self.to_vertex:
+            return self.from_vertex
         else:
             raise Exception("Error")
 
     def residual_capacity_to(self, vertex):
-        if vertex == self.from_node:
+        if vertex == self.from_vertex:
             return self.flow
-        elif vertex == self.to_node:
+        elif vertex == self.to_vertex:
             return self.capacity - self.flow
         else:
             raise Exception("Error")
 
     def add_residual_flow_to(self, vertex, flow_to_add):
-        if vertex == self.from_node:
+        if vertex == self.from_vertex:
             self.flow -= flow_to_add
-        elif vertex == self.to_node:
+        elif vertex == self.to_vertex:
             self.flow += flow_to_add
         else:
             raise Exception("Error")
 
     def __str__(self):
-        return "From " + str(self.from_node) + " to " + str(self.to_node) + "; cap: " + str(self.capacity) + "; flow: " + str(self.flow)
+        return "From " + str(self.from_vertex) + " to " + str(self.to_vertex) + "; cap: " + str(self.capacity) + "; flow: " + str(self.flow)
 
 class FlowGraph:
     def __init__(self, V):
@@ -57,8 +55,8 @@ class FlowGraph:
         self.E = 0
 
     def add_edge(self, e: FlowEdge):
-        self.adj[e.from_node].append(e)
-        self.adj[e.to_node].append(e)
+        self.adj[e.from_vertex].append(e)
+        self.adj[e.to_vertex].append(e)
         self.E += 1
 
     def has_augmenting_path_bfs(self):
@@ -117,49 +115,27 @@ class FlowGraph:
         return total_flow
 
 # Create graph and edges
-total_nodes = empty_squares + 2 * n + m + 2
-graph = FlowGraph(total_nodes)
+total_vertices = n + m + 2
+graph = FlowGraph(total_vertices)
 debug_graph_building = False
 
-# add edges from source to the row nodes with appropriate capacity representing number of empty squares in that row
-for i in range(len(trees_matrix)):
-    temp_cap = 0
-    for j in range(len(trees_matrix[0])):
-        if trees_matrix[i][j] is None:
-            temp_cap += 1
-    from_to = [0, i + 1]
-    if debug_graph_building: print(from_to, temp_cap)
-    graph.add_edge(FlowEdge(from_to[0], from_to[1], temp_cap))
-
-# make row nodes have capacity of two
-for i in range(1, 1 + n):
-    from_to = [i, i + n]
-    if debug_graph_building: print(from_to)
+# add edges from source to the row vertices with capacity of two
+for i in range(1, n + 1):
+    from_to = [0, i]
+    if debug_graph_building: print(from_to, 2)
     graph.add_edge(FlowEdge(from_to[0], from_to[1], 2))
 
-# add edges from row to the second layer of empty squares
-empty_squares_counter = 2 * n + 1
-for i in range(len(trees_matrix)):
-    for j in range(len(trees_matrix[0])):
+# add appropriate edges from row to the column vertices
+for i in range(n):
+    for j in range(m):
         if trees_matrix[i][j] is None:
-            from_to = [n + i + 1, empty_squares_counter]
+            from_to = [1 + i, 1 + n + j]
             if debug_graph_building: print(from_to)
             graph.add_edge(FlowEdge(from_to[0], from_to[1], 1))
-            empty_squares_counter += 1
 
-# add edges from second layer of empty squares to column
-empty_squares_counter = 2 * n + 1
-for i in range(len(trees_matrix)):
-    for j in range(len(trees_matrix[0])):
-        if trees_matrix[i][j] is None:
-            from_to = [empty_squares_counter, empty_squares + 2 * n + j + 1]
-            if debug_graph_building: print(from_to)
-            graph.add_edge(FlowEdge(from_to[0], from_to[1], 1))
-            empty_squares_counter += 1
-
-# add edges from column to sink t = total_nodes - 1
-for i in range(total_nodes - m - 1, total_nodes - 1):
-    from_to = [i, total_nodes - 1]
+# add edges from column vertices to sink t = total_vertices - 1
+for i in range(total_vertices - m - 1, total_vertices - 1):
+    from_to = [i, total_vertices - 1]
     if debug_graph_building: print(from_to)
     graph.add_edge(FlowEdge(from_to[0], from_to[1], 1))
 
